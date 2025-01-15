7fd299703ca1b0957f5b1d7aa2a330cb32b8ff4f
Samikshakotkar317 <samikshakotkat31@gmail.com>
2025-01-15 19:09:25 +0530

    conversion of datatypes

---
+++ 01_basics/03-conversionOperation.js
@@ -0,0 +1,29 @@
+let score = true //number
+// let score = "33abc" //string
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // if tere is no number and also undefined in score then output come NaN=not a number and for null give 0 and true give 1 or false give 0
+
+// "33" => 33
+//  "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "samiksha"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+//  "" => false
+// "samiksha" => true
+
+let someNumber = 33
+
+let stringNumber = string(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
