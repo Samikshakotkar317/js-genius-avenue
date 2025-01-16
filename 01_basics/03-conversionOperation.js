dcba970c6a400ea9c2c58d4c06ba344951cd6b30
Samikshakotkar317 <samikshakotkat31@gmail.com>
2025-01-16 06:21:44 +0530

    conversion Operations

--- 01_basics/03-conversionOperation.js
+++ 01_basics/03-conversionOperation.js
@@ -19,11 +19,47 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 // 1 => true; 0 => false
 //  "" => false
-// "samiksha" => true
+// "samiksha" => true*
 
 let someNumber = 33
 
-let stringNumber = string(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = string(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************************* Operations **********************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " samiksha"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(true);
+// console.log(+true);
+// console.log(+""); // don not use
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
 
