f113c3b9130607f043f47a05948c9bf0d1a74796
Samikshakotkar317 <samikshakotkat31@gmail.com>
2025-01-18 17:00:41 +0530

    datatypes summary program

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+// primitive
+
+// 7 typs : string, numbers, boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 1234562345678456n
+
+
+// reference (non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "daga"] //arrays
+let myObj = {
+    name: "samiksha",
+    age: 18, //in {} all are objects
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+    
+}
+
+console.log(typeof heros);
