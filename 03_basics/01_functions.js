83106a65cc4a7928d7b19538cd8c7ec460405999
Samikshakotkar317 <samikshakotkat31@gmail.com>
2025-02-18 22:31:28 +0530

    update functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,53 @@
+// console.log("S");
+// console.log("A");
+// console.log("M");
+// console.log("M");
+// console.log("Y");
+
+function sayMyName(){
+    console.log("S");
+    console.log("A");
+    console.log("M");
+    console.log("M");
+    console.log("Y");
+}
+
+// function referance => sayMyName
+// function exucution =>  sayMyName() //for argument and no in() like (4, 3)
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+    
+// }  //(number1, nomber2) these are parameters
+
+function addTwoNumbers(number1, number2){
+    // let result = number1 + number2
+    // return result
+    // console.log("sammy"); //function property - once we write return after that no value execute value does on print
+    
+    return number1 + number2
+    
+}
+
+// addTwoNumbers(3, 4)
+// addTwoNumbers(3, "4") //both act as string and give output 34
+
+const result = addTwoNumbers(3, 4)
+
+// console.log("result:", result);
+
+function loginUserMessage(username = "sam"){
+    // if(username === undefined){
+    //     console.log("Please enter a username");
+    if(!username){
+        console.log("Please enter a username");
+        return 
+    } //value chack in between () and code execute in {} // ! convert true to false and false to true 
+    return`${username} just logged in`
+}  //using $ sign we write variable name
+
+console.log(loginUserMessage("sammy"));//executing value
+// console.log(loginUserMessage(""));
+// console.log(loginUserMessage());
